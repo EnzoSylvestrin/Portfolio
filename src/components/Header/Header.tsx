85039b2c3c8be429564adfe433633ec7ee51e216
EnzoSylvestrin <enzospavani@gmail.com>
2023-01-29 23:51:18 -0300

    Adicionando responsividade no header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,13 +15,10 @@ const Header = () => {
 
     return (
         <header id="#top">
-            <nav className="w-full bg-neutral-800 fixed border-b-cyan-400 border-b-[1px] px-4 flex items-center justify-between h-[12vh] min-h-[4rem]">
-                <a href="#top" className="text-transparent text-2xl text-cyan-500">Enzo</a>
+            <nav className="w-full bg-neutral-800 fixed border-b-cyan-400 border-b-[1px] px-4 flex items-center justify-center h-[12vh] min-h-[4rem] md:justify-between">
+                <a href="#top" className="text-center text-transparent text-2xl text-cyan-500 md:text-left">Enzo</a>
                 <MediaQuery maxWidth={768}>
-                    <ul></ul>
-                </MediaQuery>
-                <MediaQuery minWidth={768}>
-                    <ul className="flex items-center justify-center gap-5 mr-4">
+                    <ul className="bg-black border-l-cyan-400 border-l-[1px] fixed top-[12vh] w-[180px] right-0 h-[88vh] flex-col flex items-center justify-center gap-5">
                         <ItemUl>
                             Home
                         </ItemUl>
@@ -31,12 +28,27 @@ const Header = () => {
                         <ItemUl>
                             Contato
                         </ItemUl>
-                    </ul>
-                    <ul className="flex items-center justify-center gap-4 mr-4 text-gray-200 md:">
-                        <ThemeComponent dark={DarkMode} onClick={ToggleMode} />
-                        <li className="text-xxl hover:text-gray-400"><AiOutlineGithub /></li>
+                        <div className='flex items-center justify-center gap-6 absolute right-0 bottom-5 w-full'>
+                            <ThemeComponent dark={DarkMode} onClick={ToggleMode} />
+                            <li className="text-xxl text-gray-200 hover:text-gray-400"><AiOutlineGithub /></li>
+                        </div>
                     </ul>
                 </MediaQuery>
+                <ul className="hidden items-center justify-center gap-5 mr-4 md:flex">
+                    <ItemUl>
+                        Home
+                    </ItemUl>
+                    <ItemUl>
+                        Projetos
+                    </ItemUl>
+                    <ItemUl>
+                        Contato
+                    </ItemUl>
+                </ul>
+                <ul className="hidden items-center justify-center gap-4 mr-4 text-gray-200 md:flex ">
+                    <ThemeComponent dark={DarkMode} onClick={ToggleMode} />
+                    <li className="text-xxl hover:text-gray-400"><AiOutlineGithub /></li>
+                </ul>
             </nav>
         </header>
     )
--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -4,7 +4,7 @@ import { BiMoon, BiSun } from 'react-icons/bi';
 
 export const ItemUl = ({ children }: { children: string }) => {
     return (
-        <li className="text-lg text-gray-200 cursor-pointer
+        <li className="text-lg text-gray-200 cursor-pointer tracking-wider
             after:block after:h-0 after:border-b-[2px] after:border-b-cyan-500 after:mr-1 after:scale-0 after:transition-transform after:duration-[320ms] after:ease-in-out
             hover:after:scale-100 hover:after:h-auto"
         >
@@ -16,7 +16,7 @@ export const ItemUl = ({ children }: { children: string }) => {
 export const ThemeComponent = ({ dark, ...rest }: LiHTMLAttributes<HTMLLIElement> & { dark: boolean }) => {
     return (
         <li className={
-            clsx("text-xxl", dark ? 'hover:text-yellow-300' : 'hover:text-black')} {...rest}
+            clsx("text-xxl text-gray-200 ", dark ? 'hover:text-yellow-300' : 'hover:text-black')} {...rest}
         >
             {dark ? <BiSun /> : <BiMoon />}
         </li>
