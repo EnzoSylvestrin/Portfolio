0df7e62f027cd62caee12715e14d9fdeb93d07ed
EnzoSylvestrin <enzospavani@gmail.com>
2023-02-01 21:42:51 -0300

    Adicionando type writer e estilizando a home

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,21 +1,32 @@
-import { MouseParallaxChild, MouseParallaxContainer } from "react-parallax-mouse";
+import Typewriter from "typewriter-effect";
 
 import Button from "../Button/Button";
 
 const HomeComponent = () => {
     return (
-        <div className="flex items-center flex-col h-full justify-center pt-[12vh] min-h-[100vh]">
-            <h1 className="text-xlg text-center dark:text-white">Seja bem vindo!<br />Eu sou o Enzo</h1>
-            <Button text="Clique aqui" styles="mt-4" />
-            <MouseParallaxContainer className="w-full flex items-center justify-center" globalFactorX={0.1} globalFactorY={0.1}>
-                <MouseParallaxChild factorX={0.3} factorY={0.5}>
-                    <img src="https://ionicframework.com/docs/icons/logo-react-icon.png" height={'200px'} width={'200px'} alt="" />
-                </MouseParallaxChild>
-                <MouseParallaxChild factorX={0.7} factorY={0.8}>
-                    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968381.png" height={'200px'} width={'200px'} alt="" />
-                </MouseParallaxChild>
-            </MouseParallaxContainer>
-        </div>
+        <>
+            <div className="flex items-center justify-center pt-[12vh] px-12 h-full">
+                <div className="flex items-center flex-col h-full w-full justify-center md:w-[50%] md:items-start">
+                    <h1 className="text-xlg mb-6 text-center dark:text-white md:text-start">Seja bem vindo!<br />meu nome é Enzo</h1>
+
+                    <Typewriter
+                        options={{
+                            strings: ['Desenvolvedor', 'FullStack', 'Junior', 'Freenlancer', 'Gamer'],
+                            autoStart: true,
+                            loop: true,
+                            cursorClassName: 'text-xlg bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-blue-500 to-cyan-400',
+                            wrapperClassName: 'text-xlg bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-blue-500 to-cyan-400'
+                        }}
+                    />
+
+                    <Button text="Me encontre" styles="mt-8 text-xl" />
+                </div>
+                <div className="w-[50%] hidden md:flex">
+
+                </div>
+            </div>
+
+        </>
     );
 }
 
