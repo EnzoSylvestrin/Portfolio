fb027d24e7c9be9aa3bde12c0e139b79a430f53e
EnzoSylvestrin <enzospavani@gmail.com>
2023-02-11 23:31:42 -0300

    Colocando cursor pointer no component de idioma

--- src/components/Language/LanguageComponent.tsx
+++ src/components/Language/LanguageComponent.tsx
@@ -8,7 +8,7 @@ const LanguageComponent = () => {
     const { language, setLanguage } = useLanguage()
 
     return (
-        <div onClick={() => setLanguage(language === "Português" ? 'English' : 'Português')} className="flex text-stone-900 bg-gray-300 items-center gap-1 justify-center rounded-lg dark:bg-slate-900 dark:text-white px-2 py-1 mb-3 transition-all duration-200 hover:scale-105">
+        <div onClick={() => setLanguage(language === "Português" ? 'English' : 'Português')} className="flex cursor-pointer text-stone-900 bg-gray-300 items-center gap-1 justify-center rounded-lg dark:bg-slate-900 dark:text-white px-2 py-1 mb-3 transition-all duration-200 hover:scale-105">
             <IoLanguage size={20} />
             <Text
                 text={{
