b19267582ebc34de64689eb523c49622ed1ed505
Enzo Sylvestrin <enzospavani@gmail.com>
2023-03-29 10:53:23 -0300

    Alterando animação dos serviços

--- src/components/sections/Services/Services.tsx
+++ src/components/sections/Services/Services.tsx
@@ -4,7 +4,6 @@ import { RiCodeSSlashFill } from "react-icons/ri";
 import { MdPhoneAndroid } from "react-icons/md";
 import { ServiceCard } from "./ServicesStyled";
 
-import { motion } from 'framer';
 
 import ContainerCommom from "@/components/ContainerCommom";
 import { TitleSection } from "@/components/CommomComponents";
@@ -13,33 +12,35 @@ const Services = () => {
     return (
         <ContainerCommom id="Servicos">
             <div>
-                <TitleSection title={{
-                    English: "Services",
-                    Portugues: "Serviços"
-                }} className="mb-4" />
-                <motion.div
+                <TitleSection
+                    title={{
+                        English: "Services",
+                        Portugues: "Serviços"
+                    }}
+                    className="mb-4"
+                />
+                <div
                     className="grid grid-cols-1 gap-6 w-full px-3 content-center mt-4 sm:grid-cols-2 md:grid-cols-3 lg:px-8"
-                    initial={{ x: 200, opacity: 0 }}
-                    whileInView={{ x: 0, opacity: 1 }}
-                    transition={{ duration: 0.4 }}
-                    viewport={{ once: true }}
                 >
                     <ServiceCard
                         icon={BsGearFill}
                         titleElement={{ Portugues: "Automação", English: "Automation" }}
+                        x={-100}
                         text={{ Portugues: "Criação de bots e automação de tarefas repetitivas.", English: "Creation of bots and automation of repetitive tasks" }}
                     />
                     <ServiceCard
                         icon={RiCodeSSlashFill}
                         titleElement={{ Portugues: "Criação de sites", English: "Website creation" }}
+                        y={-100}
                         text={{ Portugues: "Construção do design e funcionamento de sites.", English: "Construction of website design and functionality" }}
                     />
                     <ServiceCard
                         icon={MdPhoneAndroid}
                         titleElement={{ Portugues: "Criação de apps", English: "Apps development" }}
+                        x={100}
                         text={{ Portugues: "Desenvolvimento de aplicativos.", English: "Development of apps." }}
                     />
-                </motion.div>
+                </div>
             </div>
         </ContainerCommom>
     );
--- src/components/sections/Services/ServicesStyled.tsx
+++ src/components/sections/Services/ServicesStyled.tsx
@@ -2,6 +2,8 @@ import { HTMLAttributes } from "react";
 
 import { IconType } from "react-icons";
 
+import { motion } from 'framer';
+
 import Heading from "@/components/Heading";
 import Text, { LanguageProps } from "@/components/Text";
 import { LinearGradientIcon } from "@/components/CommomComponents";
@@ -10,12 +12,21 @@ type ServiceCardProps = HTMLAttributes<HTMLDivElement> & {
     icon: IconType,
     titleElement: LanguageProps,
     text: LanguageProps,
+    x?: number,
+    y?: number,
     className?: string,
 }
 
-export const ServiceCard = ({ icon, text, titleElement, className, ...rest }: ServiceCardProps) => {
+export const ServiceCard = ({ icon, text, titleElement, x, y, className, ...rest }: ServiceCardProps) => {
     return (
-        <div className={`flex Card-Gradient z-[1] relative transition-all duration-300 before:rounded-lg after:rounded-lg hover:scale-[1.02] ${className}`} {...rest}>
+        <motion.div
+            className={`flex Card-Gradient z-[1] relative transition-all duration-300 before:rounded-lg after:rounded-lg hover:scale-[1.02] ${className}`}
+            initial={{ x: x, y: y, opacity: 0 }}
+            whileInView={{ x: 0, y: 0, opacity: 1 }}
+            transition={{ duration: 0.4 }}
+            viewport={{ once: true }}
+            {...rest}
+        >
             <div className="flex w-full ml-[6px] mt-[6px] flex-col gap-3 px-4 py-6 rounded-md bg-gray-200 items-center justify-center dark:bg-zinc-900">
                 <LinearGradientIcon
                     id="10"
@@ -25,6 +36,6 @@ export const ServiceCard = ({ icon, text, titleElement, className, ...rest }: Se
                 <Heading text={titleElement} size="md" className="text-center" gradient={true} />
                 <Text text={text} size="md" className="text-center px-2" />
             </div>
-        </div>
+        </motion.div>
     )
 }
