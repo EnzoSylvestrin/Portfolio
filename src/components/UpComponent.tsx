bbfb69c51ebe35da8d0bfb7b42405da39f709afb
EnzoSylvestrin <enzospavani@gmail.com>
2023-08-24 20:08:11 -0300

    fix: up button on large screens

--- src/components/UpComponent.tsx
+++ src/components/UpComponent.tsx
@@ -38,7 +38,6 @@ const UpComponent = () => {
                     id='8'
                     Icon={BsFillCaretUpFill}
                     size={28}
-                    className='2xl:w-[42px] 2xl:h-[48px]'
                     reescale
                 />
             </div>
